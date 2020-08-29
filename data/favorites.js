9fb100a5b991c70b1f3d48ed31e02d47fe46a206
brookeperkins <brookemmperkins@gmail.com>
2020-08-28 20:18:04 -0700

    get post and test

---
+++ data/favorites.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    strdrink: 'Applecar',
+    strGlass: 'Cocktail glass',
+  },
+  {
+    strdrink: 'Rum Punch',
+    strGlass: 'Punch bowl'
+  },
+  {
+    strdrink: 'Mai Tai',
+    strGlass: 'Collins glass'
+  },
+  {
+    strdrink: 'Casa Blanca',
+    strGlass: 'Cocktail glass'
+  }
+];
