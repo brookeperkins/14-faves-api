89c63d4a4c9839e4fc9a8f315deaff8a6119c380
brookeperkins <brookemmperkins@gmail.com>
2020-08-28 20:40:20 -0700

    test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -33,7 +33,7 @@ describe('app routes', () => {
     return client.end(done);
   });
 
-  skip('returns api search results', async(done) => {
+  test('returns api search results', async(done) => {
     const expectation =
     {
       'drinks': [
